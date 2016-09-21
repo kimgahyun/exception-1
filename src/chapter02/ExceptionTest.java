968e2811dd192957ece2042988ca56ec5fdf58bb
Hokyung Lee <endlesslhk@nate.com>
2016-09-21 15:05:34 +0900

    ㅇㅇ

---
+++ src/chapter02/ExceptionTest.java
@@ -0,0 +1,28 @@
+package chapter02;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+		int a = 0;
+		int k = 0;
+		try {
+			System.out.println("파일을 열었음");
+
+			k = 1000 / a;
+			System.out.println("more code...");
+		} catch (ArithmeticException ex) {
+			// 1. 로그 남기기 (파일)
+			// 2. 사과(정상종료)
+			System.out.println("죄송합니다. 예기치 않은..." + ex);
+
+			// 3. 1~2라도 못하겠으면,
+			// ex.printStackTrace();
+
+			return;
+		} finally {
+			System.out.println("자원정리");
+		}
+		System.out.println("결과는:" + k);
+	}
+
+}
---
+++ src/chapter02/MyException.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class MyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public MyException() {
+		super("MyException Occurs");
+	}
+	
+	public MyException(String message) {
+		super(message);
+	}
+
+}
